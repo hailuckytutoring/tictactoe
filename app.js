ce3d82ab5ef962a751b19c74f2e277a7fe89ea28
Canhui Huang <canhui.huang@mobicash.mx>
2021-12-18 01:24:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+let turn = 0;
+const pieces = ['O', 'X'];
+const board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+let winner = '';
+
+function displayWinner() {
+  document.querySelector('.winner').innerHTML = `<h2>${pieces[turn]} is the winner!</h2>`;
+}
+
+function getWinner() {
+  // Horizontal Cases
+  if (board[0] === board[1] && board[0] === board[2] && board[0] !== ' ') {
+    return pieces[turn];
+  }
+  if (board[3] === board[4] && board[3] === board[5] && board[3] !== ' ') {
+    return pieces[turn];
+  }
+  if (board[6] === board[7] && board[6] === board[8] && board[6] !== ' ') {
+    return pieces[turn];
+  }
+
+  // Vertical Cases
+  if (board[0] === board[3] && board[0] === board[6] && board[0] !== ' ') {
+    return pieces[turn];
+  }
+  if (board[1] === board[4] && board[1] === board[7] && board[1] !== ' ') {
+    return pieces[turn];
+  }
+  if (board[2] === board[5] && board[2] === board[8] && board[2] !== ' ') {
+    return pieces[turn];
+  }
+
+  // Diagonal Cases
+  if (board[0] === board[4] && board[0] === board[8] && board[0] !== ' ') {
+    return pieces[turn];
+  }
+  if (board[6] === board[4] && board[6] === board[2] && board[6] !== ' ') {
+    return pieces[turn];
+  }
+
+  return '';
+}
+
+function placePiece(e) {
+  if (e.target.innerText === '') {
+    // Place the piece in the board array
+    board[Number(e.target.id) - 1] = pieces[turn];
+
+    // Place the piece in the UI
+    e.target.innerText = pieces[turn];
+
+    // Determine winner
+    winner = getWinner();
+    if (winner !== '') {
+      // Disable inputs
+      for (let i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', placePiece);
+      }
+
+      displayWinner();
+    }
+
+    // Next turn
+    turn++;
+    if (turn > 1) {
+      turn = 0;
+    }
+  }
+}
+
+// Listen for clicks on the cells
+const cells = document.querySelectorAll('.cell');
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', placePiece);
+}
